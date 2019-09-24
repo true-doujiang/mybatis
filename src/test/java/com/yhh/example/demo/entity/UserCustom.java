b72746b1b5dd9d47eebe7eb3f1b49e117dc3d1f2
youhh <16621043230@163.com>
2019-09-24 19:29:44 +0800

    add demo start

---
+++ src/test/java/com/yhh/example/demo/entity/UserCustom.java
@@ -0,0 +1,11 @@
+package com.yhh.example.demo.entity;
+
+/**
+ * @Author: -小野猪-
+ * @Date: 2019/6/30 10:35
+ * @Version: 1.0
+ * @Desc: 用户的扩展类
+ */
+public class UserCustom extends User {
+	//可以扩展用户的信息
+}
---
+++ src/test/java/com/yhh/example/demo/entity/UserQueryVo.java
@@ -0,0 +1,22 @@
+package com.yhh.example.demo.entity;
+
+
+
+/**
+ * @Author: -小野猪-
+ * @Date: 2019/6/30 10:35
+ * @Version: 1.0
+ * @Desc: 在这里包装所需要的查询条件
+ */
+public class UserQueryVo {
+
+	
+	//用户查询条件(根据需要也可以是User)
+	private UserCustom userCustom;
+
+
+	
+	//可以包装其它的查询条件，订单、商品
+	//....
+	
+}
